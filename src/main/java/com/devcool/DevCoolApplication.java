3e8cf514deedbcf6e7553de0d58a78e4b97f4b34
Hien Giang <quachtrieuvu@gmail.com>
2025-10-05 18:08:19 +0700

    deployment: containerized postgre db & pg admin

---
+++ src/main/java/com/devcool/DevCoolApplication.java
@@ -0,0 +1,16 @@
+package com.devcool;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevCoolApplication {
+    private static final Logger log = LoggerFactory.getLogger(DevCoolApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevCoolApplication.class, args);
+	}
+
+}
--- src/test/java/com/devcool/DevCoolApplicationTests.java
+++ src/test/java/com/devcool/DevCoolApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DevcoolApplicationTests {
+class DevCoolApplicationTests {
 
 	@Test
 	void contextLoads() {
