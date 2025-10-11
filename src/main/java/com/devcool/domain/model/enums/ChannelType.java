e51d2fdeab835eacd0ff66ecb4645b5c4d5dcf73
Hien Giang <94091904+hinn1411@users.noreply.github.com>
2025-10-12 01:53:17 +0700

    implement: domain model (#10)

---
+++ src/main/java/com/devcool/domain/model/enums/ChannelType.java
@@ -0,0 +1,7 @@
+package com.devcool.domain.model.enums;
+
+public enum ChannelType {
+    LOUNGE,
+    FORUM,
+    PRIVATE_CHAT
+}
---
+++ src/main/java/com/devcool/domain/model/enums/MemberType.java
@@ -0,0 +1,6 @@
+package com.devcool.domain.model.enums;
+
+public enum MemberType {
+    MEMBER,
+    LEADER
+}
