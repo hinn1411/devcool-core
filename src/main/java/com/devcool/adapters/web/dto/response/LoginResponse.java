a8f33447ad64cbe8d7c6366600ddcf537029e586
Hien Giang <quachtrieuvu@gmail.com>
2025-11-01 23:59:25 +0700

    refactor: add getters and setters

--- src/main/java/com/devcool/adapters/web/dto/response/LoginResponse.java
+++ src/main/java/com/devcool/adapters/web/dto/response/LoginResponse.java
@@ -1,10 +1,16 @@
 package com.devcool.adapters.web.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
+import lombok.*;
 
 @Schema(name = "LoginResponse", description = "Login result payload")
 @Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class LoginResponse {
     @Schema(description = "Unique identifier of the user", example = "101")
     private String accessToken;
