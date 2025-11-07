f4cbee96a79173329ee803e7d7d854b7cd7e0bc9
Hien Giang <quachtrieuvu@gmail.com>
2025-11-07 23:05:18 +0700

    test: update ci

--- src/main/java/com/devcool/adapters/web/controller/AuthController.java
+++ src/main/java/com/devcool/adapters/web/controller/AuthController.java
@@ -29,7 +29,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,6 +36,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @Tag(name = "Auth")
 @RestController
